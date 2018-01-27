2cb5e6770da8082f71eb4edd3845c5dfa57373df
c1fr1 <c1fr1.stkcps@gmail.com>
2018-01-27 03:50:44 +0000

    added functionality for set

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class Player {
+
     public String name;
+
     public double currentRank;
-    public List<Set> setHistory;
+
+    public ArrayList<Set> setHistory;
+
     public Player(String n) {
         name = n;
         currentRank = 2;
